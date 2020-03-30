4d140151003ad3b371e8421717749e8df21e8fef
ben <ben.franklin@hidglobal.com>
2020-03-30 18:30:50 +0530

    graphql done

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
-const express=require('express');
-const graphqlHTTP=require('express-graphql');
-const app=express();
-const schema=require('./schema/schema')
-app.use('/graphql',graphqlHTTP({
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const app = express();
+const schema = require('./schema/schema')
+const Mongoose=require('mongoose');
+const dbURI='mongodb+srv://ben:ben12345@cluster0-o79ou.mongodb.net/test?retryWrites=true&w=majority';
+Mongoose.connect(dbURI,{ useNewUrlParser: true ,useUnifiedTopology: true } ,function(err){    
+    if(err){
+    console.log('Some problem with the connection ' +err)   
+    } 
+    else {
+    console.log('The Mongoose connection is ready')  
+    }
+})
+
+app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql:true
+    graphiql: true
 }))
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log("now listening on port 4000")
 })
