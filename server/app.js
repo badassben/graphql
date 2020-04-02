68e2d13240bf3a0b52cedae633db3059a03d3600
ben <ben.franklin@hidglobal.com>
2020-04-02 13:23:17 +0530

    changes done

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const graphqlHTTP = require('express-graphql');
 const app = express();
 const schema = require('./schema/schema')
 const Mongoose=require('mongoose');
-import {graphqlExpress,graphiqlExpress}from 'apollo-server-express';
 const dbURI='mongodb+srv://ben:ben12345@cluster0-o79ou.mongodb.net/test?retryWrites=true&w=majority';
 Mongoose.connect(dbURI,{ useNewUrlParser: true ,useUnifiedTopology: true } ,function(err){    
     if(err){
@@ -18,7 +17,6 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql:true
 }))
-app.use('/',graphiqlExpress({endpointURL:'/graphql'}))
 app.listen(process.env.PORT || 4000, () => {
-    console.log(`Server started on port: ${process.env.PORT || 3000}`);
+    console.log(`Server started on port: ${process.env.PORT || 4000}`);
   });
